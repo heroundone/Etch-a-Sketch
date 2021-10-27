f90217f6c2b079657b44fc9bc6653a41c5388cdb
Jason Bedini <jasonbedini@gmail.com>
2021-10-27 14:00:15 -0700

    added rainbow coloring to script.js, removed rule from style.css

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 let resetBtn = document.createElement('button');
 resetBtn.innerText = 'Clear Grid';
 resetBtn.addEventListener('click', function () {
+    // delete the old grid
     let divsToDelete = document.querySelectorAll('#container div'); // obtain node list of divs within grid
     removeGrid(divsToDelete);
     
+    // prompt for new grid dimensions
     let gridSideLength = prompt('How many squares per side in the new grid?'); // get size of grid sides
     gridSideLength = Math.round(gridSideLength);
     while (gridSideLength < 1 || gridSideLength > 100) { // check user's input
@@ -12,10 +14,9 @@ resetBtn.addEventListener('click', function () {
         gridSideLength = math.round(gridSideLength);
     }; 
     gridSize = Math.pow(gridSideLength, 2); // calculate total number of divs in the grid
-    console.log(gridSize);
     createGrid(container, gridSize, gridSideLength, containerHeight, containerWidth); // create new grid
 });
-document.body.appendChild(resetBtn); // finally add button to top of page
+document.body.appendChild(resetBtn);
 
 // create the container, which will contain the divs to form the grid
 const container = document.createElement('div');
@@ -26,7 +27,7 @@ document.body.appendChild(container);
 containerHeight = 650;
 containerWidth = 900;
 
-// initially how many squares per side (initial value will be 16)
+// initialize how many squares per side
 let gridSize = 256;
 let gridSideLength = 16;
 
@@ -50,7 +51,17 @@ function createGrid (container, gridSize, gridSideLength, containerHeight, conta
     // iterate through grid and append each div to a document fragment, then append fragment to container
     for (let i = 1; i <= gridSize; i++) {
         let clone = div.cloneNode();
-        clone.addEventListener('mouseenter', (e) => e.target.classList.add('hover')); // mouse turns div black
+        // Add event listener to allow coloring in of grid, also checks if a specific div is already colored
+        clone.addEventListener('mouseenter', function(e) {
+            let id = e.target.getAttribute('id');
+            if (id !== 'colored') {
+                let red = getRGB(0, 255);
+                let green = getRGB(0, 255);
+                let blue = getRGB(0, 255);
+                e.target.style.background = 'rgb' + `(${red}, ${green}, ${blue})`
+                e.target.setAttribute('id', 'colored');
+            };
+        });
         fragment.appendChild(clone);
     }; 
     container.appendChild(fragment);
@@ -63,6 +74,14 @@ function removeGrid(divsToDelete) {
         div.remove()
     });
 };
+
+// function that will allow a random rgb value between (0) and (255) to be generated
+function getRGB(min, max) {
+    lowerBound = Math.ceil(min);
+    upperBound = Math.floor(max);
+    return Math.floor(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
+    
+}
 
 // call function to create initial grid
 createGrid(container, gridSize, gridSideLength, containerHeight, containerWidth);
